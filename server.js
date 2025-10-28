7ea766ac5df1f5324ced7c87614affbacb96c6e6
Sajal Singh <as3199432@gmail.com>
2025-10-28 22:22:51 +0530

    Add socket event handlers for user connection and message logging

--- server.js
+++ server.js
@@ -6,8 +6,18 @@ const httpServer = createServer(app);
 const io = new Server(httpServer, { /* options */ });
 
 io.on("connection", (socket) => {
-  // ...
+  console.log("User connected");
+
+  socket.on("disconnect", () => {
+    console.log("A user disconnected")
+  });
+  
+  socket.on("message",() =>{
+    console.log("Message received")
+  })
+
 });
+
 
 httpServer.listen(3000, () => {
   console.log("Server is runnig on port number 3000");
