7cf5869f2f31e3295a61ebdddaa9c78b1add938c
Sajal Singh <as3199432@gmail.com>
2025-11-12 00:02:44 +0530

    apply auth for socket.io

--- server.js
+++ server.js
@@ -3,18 +3,6 @@ const app = require("./src/app");
 const connectDB = require("./src/db/db")
 const initSocketServer = require("./src/sockets/socket.server")
 const httpServer = require('http').createServer(app);
-// const generateResponse = require("./src/service/ai.service")
-
-
-//   socket.on("ai-message", async (data) =>{
-    
-//     console.log("received AI-Message")
-//     const response = await generateResponse(data.prompt);
-//     console.log("AI Response:", response);
-//     socket.emit("ai-message-response",{response})
-//   })
-
-// });
 
 connectDB() 
 initSocketServer(httpServer)
--- src/sockets/socket.server.js
+++ src/sockets/socket.server.js
@@ -1,8 +1,33 @@
 const { Server } = require("socket.io");
+const cookie = require("cookie");
+const userModal = require("../model/user.model")
+const jwt = require("jsonwebtoken");
 
 function initSocketServer(httpServer) {
 
-    const io = new Server(httpServer, { /* options */ });
+    const io = new Server(httpServer, {});
+
+    io.use(async(socket, next) => {
+
+        const cookies = cookie.parse(socket.handshake.headers?.cookie || "");
+
+        if (!cookies.token) {
+            next(new Error("Authentication error: No token provided"))
+        }
+
+        try {
+
+            const decoded = jwt.verify(cookies.token, process.env.JWT_SECRET);
+
+            const user = await userModel.findById(decoded.id);
+
+            socket.user = user;
+            next()
+
+        } catch (err){
+            next( new Error("Authentication error: Invalid token"));
+        }
+    })
 
     io.on("connection", (socket) => {
         console.log("New Socket connection:", socket.id);
