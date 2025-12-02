2f3f68dcf6b3428c75729a1936948b154dc3989e
Sajal Singh <130673366+SazzzNiziyan@users.noreply.github.com>
2025-12-02 17:37:48 +0530

    Update app.js

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -2,7 +2,7 @@ const express = require("express")
 const cookieParser = require('cookie-parser')
 const cors = require('cors')
 const path = require('path')
-
+ 
 /* routes */
 const authRoutes = require('./routes/auth.routes');
 const chatRoutes = require("./routes/chat.routes");
